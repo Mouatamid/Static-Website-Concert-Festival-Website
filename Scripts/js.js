058a116388d6aff656025257509aafa8cb536e64
Mouatamid <mouatamid.ela@gmail.com>
2019-11-14 17:03:15 +0100

    Create the header and the navbar-FIX

--- Scripts/js.js
+++ Scripts/js.js
@@ -1,37 +1,41 @@
 //Search bar focus
-let inputSearch = document.getElementById('search');
-let searchBar = document.getElementsByClassName('search-bar')[0];
-let nav = document.getElementsByTagName('nav')[0];
-inputSearch.addEventListener('focus', () => {
+let inputSearch = document.getElementById("search");
+let searchBar = document.getElementsByClassName("search-bar")[0];
+let nav = document.getElementsByTagName("nav")[0];
+inputSearch.addEventListener("focus", () => {
   searchBar.classList.add("search-barOnfocus");
 });
-inputSearch.addEventListener('blur', () => {
+inputSearch.addEventListener("blur", () => {
   searchBar.classList.remove("search-barOnfocus");
-})
+});
 
 //Nav on scroll
-window.addEventListener('scroll', () => {
-  if(window.scrollY > 180){
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 180) {
     nav.classList.add("navScroll");
-  }
-  else{
+  } else {
     nav.classList.remove("navScroll");
   }
-})
+});
 
 //Displaying the nav-list when clicking the menu
 let listDisplayed = false;
 let clickedMenu = document.querySelector(".hamburger-menu a");
 let navList = document.querySelector(".nav-list");
-clickedMenu.addEventListener('click', () => {
-  if(!listDisplayed){
-    navList.style.display = "inline-block";
-    listDisplayed = true;
-    console.log(listDisplayed);
-  }
-  else{
-    navList.style.display = "none";
-    listDisplayed = false;
-    console.log(listDisplayed);
+window.addEventListener('resize', ()=>{
+  if (window.innerWidth < 1200){
+    navList.classList.add('nav-list-mobile');
   }
 })
+clickedMenu.addEventListener("click", () => {
+    if (!listDisplayed) {
+      navList.classList.remove('nav-list-mobile');
+      listDisplayed = true;
+      console.log(listDisplayed);
+    } else {
+      navList.classList.add('nav-list-mobile');
+      listDisplayed = false;
+      console.log(listDisplayed);
+    }
+  
+});
